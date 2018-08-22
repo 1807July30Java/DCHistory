7653f5309232e08a904bcf712192fe97295312a7
OmarGuz5 <omarguz225@gmail.com>
2018-08-22 15:08:24 -0400

    Added Character list

---
+++ mavenBackend/src/main/webapp/Character.java
@@ -0,0 +1,16 @@
+package com.revature.service;
+
+public class Character {
+	private String name;
+	private String age;
+	private String powers;
+	private String gender;
+	
+	public Character(String name, String age, String powers, String gender) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.powers = powers;
+		this.gender = gender;
+	}
+}
